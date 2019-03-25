a25eba6e28583923cac8580f70a2a98193dad17b
Joakim Riedel Möller <jcemoller@gmail.com>
2019-03-25 21:13:11 +0100

    Added tests. Closes #1

--- src/CultureSwitcher.cs
+++ src/CultureSwitcher.cs
@@ -9,6 +9,13 @@ namespace Monotio.Util
 
         public CultureSwitcher(string culture)
         {
+            if (!CultureInfo.Equals(CultureInfo.CurrentCulture, CultureInfo.CurrentUICulture))
+            {
+                throw new InvalidOperationException(
+                    "Different CurrentCulture and CurrentUICulture culture is not supported."
+                    );
+            }
+
             _originalCulture = CultureInfo.CurrentCulture;
             var cultureInfo = String.IsNullOrEmpty(culture) ? CultureInfo.CurrentCulture : new CultureInfo(culture);
             
--- test/CultureSwitcher.cs
+++ test/CultureSwitcher.cs
@@ -10,15 +10,31 @@ namespace Monotio.Util.Tests
         public void SwitchCulture()
         {
             CultureInfo.CurrentCulture = CultureInfo.CreateSpecificCulture("en-US");
+            CultureInfo.CurrentUICulture = CultureInfo.CreateSpecificCulture("en-US");
 
             Assert.Equal("en-US", CultureInfo.CurrentCulture.Name);
+            Assert.Equal("en-US", CultureInfo.CurrentUICulture.Name);
 
             using (new CultureSwitcher("sv-SE"))
             {
                 Assert.Equal("sv-SE", CultureInfo.CurrentCulture.Name);
+                Assert.Equal("sv-SE", CultureInfo.CurrentUICulture.Name);
             }
 
             Assert.Equal("en-US", CultureInfo.CurrentCulture.Name);
+            Assert.Equal("en-US", CultureInfo.CurrentUICulture.Name);
+        }
+
+        [Fact]
+        public void ThrowIfUICultureAndCultureDiffers()
+        {
+            CultureInfo.CurrentCulture = CultureInfo.CreateSpecificCulture("en-US");
+            CultureInfo.CurrentUICulture = CultureInfo.CreateSpecificCulture("sv-SE");
+
+            Assert.Equal("en-US", CultureInfo.CurrentCulture.Name);
+            Assert.Equal("sv-SE", CultureInfo.CurrentUICulture.Name);
+
+            Assert.Throws<InvalidOperationException>(() => new CultureSwitcher("de-DE"));
         }
     }
 }
